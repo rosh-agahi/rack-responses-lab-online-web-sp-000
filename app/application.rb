1371205d81c6683c891e538e9c35250a5528aeda
Roshanak Agahi <rosh.agahi@gmail.com>
2019-12-20 05:27:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
     resp = Rack::Response.new 
     resp.write "Current time: #{Time.now}"
     
-    if time.now.hour < 12
+    if Time.now.hour < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
